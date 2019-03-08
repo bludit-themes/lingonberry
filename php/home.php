d528dcafec311cb75ae36d14590ee339871a51cd
Edi <egoetschel@clickwork.ch>
2019-03-08 14:58:45 +0100

    Update home.php

--- php/home.php
+++ php/home.php
@@ -1,35 +1,35 @@
 <div class="content section-inner">
 	<div class="posts">
-	<?php foreach($pages as $Post): ?>
+	<?php foreach($content as $page): ?>
 		<div class="post format-standard">
 			<div class="post-bubbles">
-				<a href="<?php echo $Post->permalink() ?>" class="format-bubble" title="<?php echo $Post->title() ?>"></a>
+				<a href="<?php echo $page->permalink() ?>" class="format-bubble" title="<?php echo $page->title() ?>"></a>
 			</div>
 			<div class="content-inner">
 				<div class="post-header">
 				
-					<?php if($Post->coverImage()) {
+					<?php if($page->coverImage()) {
 							echo '<div class="featured-media">';
-							echo 	'<img class="attachment-post-image wp-post-image cover-image" src="'.$Post->coverImage().'" alt="Cover Image">';
+							echo 	'<img class="attachment-post-image wp-post-image cover-image" src="'.$page->coverImage().'" alt="Cover Image">';
 							echo		'<div class="media-caption-container">';
-							echo			'<a href="'.$Post->permalink().'" rel="bookmark" title="'.$Post->title().'">';
-							echo				'<p class="media-caption">'.$Post->description().'</p>';
+							echo			'<a href="'.$page->permalink().'" rel="bookmark" title="'.$page->title().'">';
+							echo				'<p class="media-caption">'.$page->description().'</p>';
 							echo			'</a>';
 							echo		'</div>';
 							echo '</div> <!-- /featured-media -->';
 					} ?>
 					
-					<h2 class="post-title"><a href="<?php echo $Post->permalink() ?>" rel="bookmark" title="<?php echo $Post->title() ?>"><?php echo $Post->title() ?></a></h2>
+					<h2 class="post-title"><a href="<?php echo $page->permalink() ?>" rel="bookmark" title="<?php echo $page->title() ?>"><?php echo $page->title() ?></a></h2>
 					<div class="post-meta">
 
-						<span class="post-date"><a href="<?php echo $Post->permalink() ?>" title="<?php echo $Post->date() ?>"><?php echo $Post->date() ?></a></span>
+						<span class="post-date"><a href="<?php echo $page->permalink() ?>" title="<?php echo $page->date() ?>"><?php echo $page->date() ?></a></span>
 						<span class="date-sep"> / </span>
 						<span class="post-author"><?php
-							if( Text::isNotEmpty($Post->user('firstName')) || Text::isNotEmpty($Post->user('lastName')) ) {
-								echo $Post->user('firstName').' '.$Post->user('lastName');
+							if( Text::isNotEmpty($page->user('firstName')) || Text::isNotEmpty($page->user('lastName')) ) {
+								echo $page->user('firstName').' '.$page->user('lastName');
 							}
 							else {
-								echo $Post->user('username');
+								echo $page->user('username');
 							}
 						?></span>
 												
@@ -37,10 +37,10 @@
 				</div>
 				
 				<div class="post-content">
-					<?php echo $Post->content(false) ?>
+					<?php echo $page->content(false) ?>
 					
-					<?php if($Post->readMore()) { ?>
-						<a class="read-more" href="<?php echo $Post->permalink() ?>"><?php $Language->printMe('Read more') ?></a>
+					<?php if($page->readMore()) { ?>
+						<a class="read-more" href="<?php echo $page->permalink() ?>"><?php $Language->printMe('Read more') ?></a>
 					<?php } ?>
 					
 				</div>
@@ -58,4 +58,3 @@
 		<div class="clear"></div>
 	</div>
 </div>
-
